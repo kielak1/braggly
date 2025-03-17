454805daec3a888a2a83fb17a4dea0995e0b47fa
Tadeusz Kielak <tadeusz@kielak.com>
2025-03-17 21:57:00 +0000

    drobna korekta w Authconfig (swagger)

--- backend/src/main/java/com/example/backend/controller/AuthController.java
+++ backend/src/main/java/com/example/backend/controller/AuthController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
