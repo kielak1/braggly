871d5fd81b1edd52e27d1cc5fcb96246477c3ee7
Tadeusz Kielak <tadeusz@kielak.com>
2025-04-13 14:21:58 +0000

    batch sieze 500

--- backend/src/main/java/com/example/backend/service/CodImportService.java
+++ backend/src/main/java/com/example/backend/service/CodImportService.java
@@ -118,7 +118,7 @@ public class CodImportService {
 
                 Iterator<CSVRecord> iterator = csvParser.iterator();
                 List<CSVRecord> batch = new ArrayList<>();
-                int batchSize = 10000;
+                int batchSize = 500;
                 int processed = 0;
                 log.info(String.format("[IMPORT] Batch size = %d", batchSize));
 
